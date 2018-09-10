2d44c4cdf00b60c2b63233cd25d8dc9db3bbe5b1
flyco2016 <17727820013@163.com>
2018-09-10 20:05:47 +0800

    常用的模块，去掉异常捕捉，让错误抛出试程序中断

--- get_token.py
+++ get_token.py
@@ -1,10 +1,8 @@
 import login
 
 def getToken(user_name='17727820013', password='123456'):
-    try:
-        return login.login(login_num=user_name, password=password)['data']['token']
-    except Exception as err:
-        print(err)
+    return login.login(login_num=user_name, password=password)['data']['token']
+
 
 if __name__ == '__main__':
     print(getToken())
